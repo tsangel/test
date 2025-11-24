27725d0b1fd20c7bf6a97ab0aeeb7978603cb818
Kim, Tae-Sung <taesung.angel@gmail.com>
2025-11-24 20:36:08 +0900

    fix build bench error

--- build_bench.bat
+++ build_bench.bat
@@ -8,7 +8,7 @@ if "%ROOT_DIR:~-1%"=="\" set ROOT_DIR=%ROOT_DIR:~0,-1%
 set BUILD_DIR=%ROOT_DIR%\build
 set CONFIG=RelWithDebInfo
 
-cmake -S "%ROOT_DIR%" -B "%BUILD_DIR%" -DCMAKE_BUILD_TYPE=%CONFIG% -DBUILD_SHARED_LIBS=OFF -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
+cmake -S "%ROOT_DIR%" -B "%BUILD_DIR%" -A x64 -DCMAKE_BUILD_TYPE=%CONFIG% -DBUILD_SHARED_LIBS=OFF -DCMAKE_MSVC_RUNTIME_LIBRARY=MultiThreaded
 cmake --build "%BUILD_DIR%" --config %CONFIG%
 
 set LIB_PATH=
@@ -29,6 +29,6 @@ echo Building %OUT_BIN%
 
 cl /std:c++20 /utf-8 /O2 /Zi /MT /EHsc /I"%ROOT_DIR%\include" /I"%BUILD_DIR%" /I"%BUILD_DIR%\generated\include" "%ROOT_DIR%\benchmarks\read_all_dcm.cpp" "%LIB_PATH%" /Fe:"%OUT_BIN%"
 
-echo Done -> %OUT_BIN%
+echo Done ^> %OUT_BIN%
 
 endlocal
