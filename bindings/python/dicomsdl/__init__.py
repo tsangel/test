8da6cef5db6d9556aa8355e4df024cf195677fdf
Kim, Tae-Sung <taesung.angel@gmail.com>
2026-02-22 14:57:55 +0900

    improve python import

--- bindings/python/dicomsdl/__init__.py
+++ bindings/python/dicomsdl/__init__.py
@@ -1,15 +1,29 @@
 # encoding=utf-8
 
-from . import _dicomsdl as _dicomsdl  # noqa: F401
-from ._dicomsdl import *  # noqa: F401,F403
+import importlib as _importlib
+import importlib.util as _importlib_util
+import sys as _sys
+
+_native_module_name = f"{__name__}._dicomsdl"
+if _native_module_name in _sys.modules:
+    _dicomsdl = _sys.modules[_native_module_name]
+elif _importlib_util.find_spec(_native_module_name) is not None:
+    _dicomsdl = _importlib.import_module("._dicomsdl", __name__)
+else:
+    _dicomsdl = _importlib.import_module("_dicomsdl")
+    _sys.modules.setdefault(_native_module_name, _dicomsdl)
+
+_export_names = getattr(
+    _dicomsdl,
+    "__all__",
+    tuple(name for name in vars(_dicomsdl) if not name.startswith("_")),
+)
+globals().update({name: getattr(_dicomsdl, name) for name in _export_names})
+
 from . import _image as _image  # noqa: F401
 
 DICOM_STANDARD_VERSION = getattr(_dicomsdl, "DICOM_STANDARD_VERSION", "")
 DICOMSDL_VERSION = getattr(_dicomsdl, "DICOMSDL_VERSION", "")
 __version__ = getattr(_dicomsdl, "__version__", DICOMSDL_VERSION)
 
-__all__ = getattr(
-    _dicomsdl,
-    "__all__",
-    tuple(name for name in globals() if not name.startswith("_")),
-)
+__all__ = tuple(_export_names)
